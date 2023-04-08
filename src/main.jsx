99f98327582d396d2767f704872e696582e6167c
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-04-09 03:13:39 +0600

    set header component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Components/Home/Home'
+import SlideBar from './Components/SlideBar/SlideBar'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Home></Home>
+  },
+  {
+    path:'/slider',
+    element:<SlideBar></SlideBar>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
